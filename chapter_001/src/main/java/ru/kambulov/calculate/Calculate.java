c2cb8f7b1175987f8f09a8c5d8e8a591f1756a0a
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-09 14:16:17 +0300

    1.3.JavDoc. Code convention

--- chapter_001/src/main/java/ru/kambulov/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/kambulov/calculate/Calculate.java
@@ -1,5 +1,10 @@
 package ru.kambulov.calculate;
-
+/**
+Метод main в данном классе является исполняющим программу
+сама программа выводит на консоль текстовое сообщение Hello wonderful world!
+@author Kambulov Ivan
+@since version 1.0
+*/
 public class Calculate{
 	
 	public static void main(String[] args){
