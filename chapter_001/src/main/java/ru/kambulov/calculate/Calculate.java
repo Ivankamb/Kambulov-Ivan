e53cfa89b6806c7abf98debb2398002163e190c4
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-11 14:10:25 +0300

    Changes in pom.xml

--- chapter_001/src/main/java/ru/kambulov/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/kambulov/calculate/Calculate.java
@@ -1,23 +1,23 @@
 package ru.kambulov.calculate;
 /**
-Метод main в данном классе является исполняющим программу
-сама программа выводит на консоль текстовое сообщение Hello wonderful world!
-@author Kambulov Ivan
-@since version 1.0
-*/
+ Метод main в данном классе является исполняющим программу
+ сама программа выводит на консоль текстовое сообщение Hello wonderful world!
+ @author Kambulov Ivan
+ @since version 1.0
+ */
 public class Calculate {
 	/**
-	Main.
-	@param args - args.
-	*/
+	 Main.
+	 @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello wonderful world!");
-		}
+	}
 	/**
-	Method echo.
-	@param name Your name.
-	@return Echo plus your name.
-	*/
+	 Method echo.
+	 @param name Your name.
+	 @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
--- chapter_001/src/test/java/ru/kambulov/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/kambulov/calculate/CalculateTest.java
@@ -5,27 +5,25 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-*Test
-*@author Kambulov Ivan (mailto:kia289@mail.ru)
-*@version $Id$
-*@since 0.1
-*/
+ *Test
+ *@author Kambulov Ivan (mailto:kia289@mail.ru)
+ *@version $Id$
+ *@since 0.1
+ */
 
 public class CalculateTest {
-/**
-*Test echo
-*@Test
-*метод назван по принципу when...Then...
-*/
-		public void whenTakeNameThenThreeEchoPlusName() {
+
+	@Test
+
+	public void whenTakeNameThenThreeEchoPlusName() {
 		String input = "Kambulov Ivan";
-	    String expect = "Echo, echo, echo : Kambulov Ivan"; 
-	    Calculate calc = new Calculate();
+		String expect = "Echo, echo, echo : Kambulov Ivan";
+		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
-		
-		}
-		
-		
+
+	}
+
+
 }
 
