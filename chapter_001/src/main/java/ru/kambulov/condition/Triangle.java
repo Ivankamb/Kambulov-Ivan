53f6204df0f579a6585b25a0febdb9b6ff5b90d5
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-15 10:54:00 +0300

    4.3. Вычисление площади треугольника(Правка 2.0)

--- chapter_001/src/main/java/ru/kambulov/condition/Triangle.java
+++ chapter_001/src/main/java/ru/kambulov/condition/Triangle.java
@@ -45,16 +45,6 @@ public class Triangle {
      * @return
      */
     private boolean exist(double ab, double ac, double bc) {
-        if (ab < ac + bc) {
-            if (ac < ab + bc) {
-                if (bc < ab + ac) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return ((ab < ac + bc) && (ac < ab + bc) && (bc < ab + ac));
     }
-
-
-
 }
