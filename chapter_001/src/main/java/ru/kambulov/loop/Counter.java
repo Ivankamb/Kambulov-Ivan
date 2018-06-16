0ad68a7913fc55494758b347915758da8d444f65
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-16 11:53:36 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне(2.0)

--- chapter_001/src/main/java/ru/kambulov/loop/Counter.java
+++ chapter_001/src/main/java/ru/kambulov/loop/Counter.java
@@ -1,6 +1,15 @@
 package ru.kambulov.loop;
-
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Counter {
+    /**
+     * @param start Число начала отсчёта.
+     * @param finish последнее число в диапазоне.
+     * @return сумма всех чётных в диапазоне.
+     */
 
     public int add(int start, int finish) {
         int summ = 0;
