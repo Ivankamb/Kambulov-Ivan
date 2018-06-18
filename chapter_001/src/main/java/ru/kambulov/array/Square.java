20c768b5a1bef0600dcc15a0cb8c8759c2afb9a9
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-18 14:07:33 +0300

    6.0. Заполнить массив степенями чисел.

--- chapter_001/src/main/java/ru/kambulov/array/Square.java
+++ chapter_001/src/main/java/ru/kambulov/array/Square.java
@@ -1,6 +1,15 @@
 package ru.kambulov.array;
+/**
+ *@author Kambulov Ivan (mailto:kia289@mail.ru)
+ *@version $Id$
+ *@since 0.1
+ */
 
 public class Square {
+    /**
+     * @param bound параметр указывающий количесво чисел в массиве.
+     * @return возвращает массив со степенями всех чисел по порядку.
+     */
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int count = 0; count < rst.length; count++) {
--- chapter_001/src/test/java/ru/kambulov/array/SquareTest.java
+++ chapter_001/src/test/java/ru/kambulov/array/SquareTest.java
@@ -3,6 +3,11 @@ package ru.kambulov.array;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+/**
+ *@author Kambulov Ivan (mailto:kia289@mail.ru)
+ *@version $Id$
+ *@since 0.1
+ */
 
 public class SquareTest {
     @Test
