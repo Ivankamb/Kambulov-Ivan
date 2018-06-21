89e38d2eb72ebb8a5f6d8ffe1ca23159e0a9aab2
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-21 09:39:44 +0300

    6.5. Создать программу для сортиро [195] (правка 1)

--- chapter_001/src/main/java/ru/kambulov/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/kambulov/array/BubbleSort.java
@@ -5,16 +5,15 @@ package ru.kambulov.array;
  * @since 0.1
  */
 public class BubbleSort {
-    int tmp; //перменная необходимая для перестановки индексов в массиве.
     /**
      * @param array неотсортированный массив.
      * @return отсортированный массив.
      */
     public int[] sort(int[] array) {
         for (int index = 0; index < array.length - 1; index++) {   //кол-во проходов равно кол-ву ячеек в массиве.
-            for (int j = 1; j < array.length; j++) {    //сортировка от меньшего к большему.
+            for (int j = 1; j < array.length - index; j++) {    //сортировка от меньшего к большему.
                 if (array[j] < array[j - 1]) {
-                    tmp = array[j];
+                    int tmp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = tmp;
                 }
