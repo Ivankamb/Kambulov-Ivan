bdb0d325030a2a9258c83e5ccf9e290d8338a166
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-22 11:22:48 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.

---
+++ chapter_001/src/main/java/ru/kambulov/array/MatrixCheck.java
@@ -0,0 +1,22 @@
+package ru.kambulov.array;
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixCheck {
+    /**
+     * @param data матрица с булевыми значениями.
+     * @return проверку диагоналей на однотипность.
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[0][0] != data[i + 1][i + 1] | data[data.length - 1][0] != data[data.length - (i + 2) ][i + 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/kambulov/array/MatrixCheckTest.java
@@ -0,0 +1,32 @@
+package ru.kambulov.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixCheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, false, true},
+                {false, true, false},
+                {true, true, true}};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, true, false},
+                {false, false, true},
+                {true, false, true}};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
