3ee52c0003f9d3fd7c2a7080d7af2f66ff04541c
Ivan Kambulov <mailto:kia289@mail.ru>
2018-06-28 20:55:05 +0300

    Проблема с тестовым заданием 2.

--- chapter_001/src/main/java/ru/kambulov/array/ArrayAssociation.java
+++ chapter_001/src/main/java/ru/kambulov/array/ArrayAssociation.java
@@ -17,25 +17,28 @@ public class ArrayAssociation {
         int arrayIndex2 = 0;
         int arrayIndex3 = 0;
 
-        while (arrayIndex1 < array1.length && arrayIndex2 < array2.length) {
+        while (arrayIndex1 < array1.length || arrayIndex2 < array2.length) {
             if (array1[arrayIndex1] <= array2[arrayIndex2]) {
                 array3[arrayIndex3] = array1[arrayIndex1];
                 arrayIndex1++;
-            } else {
-                array3[arrayIndex3] = array2[arrayIndex2];
-                arrayIndex2++;
+                if (arrayIndex1 == array1.length - 1) {
+                    while (arrayIndex2 < array2.length) {
+                        array3[arrayIndex3] = array2[arrayIndex2];
+                        arrayIndex2++;
+                        arrayIndex3++;
+                    }
+                } else {
+                    array3[arrayIndex3] = array2[arrayIndex2];
+                    arrayIndex2++;
+                    if (arrayIndex2 == array2.length - 1) {
+                        while (arrayIndex1 < array1.length) {
+                            array3[arrayIndex3] = array1[arrayIndex1];
+                            arrayIndex1++;
+                            arrayIndex3++;
+                        }
+                    }
+                }
             }
-            arrayIndex3++;
-        }
-        while (arrayIndex1 < array1.length) {
-            array3[arrayIndex3] = array1[arrayIndex1];
-            arrayIndex1++;
-            arrayIndex3++;
-        }
-        while (arrayIndex2 < array2.length) {
-            array3[arrayIndex3] = array2[arrayIndex2];
-            arrayIndex2++;
-            arrayIndex3++;
         }
         return array3;
     }
