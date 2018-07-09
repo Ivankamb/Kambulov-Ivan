a55a4b7d5759bbff52d3f30d301fd6653ac6a0ce
Ivan Kambulov <mailto:kia289@mail.ru>
2018-07-09 20:12:05 +0300

    Крестики-нолики на JavaFX [#49777] Логика на Predicate.

--- chapter_001/src/main/java/ru/kambulov/ticTacToe/Logic3T.java
+++ chapter_001/src/main/java/ru/kambulov/ticTacToe/Logic3T.java
@@ -1,5 +1,7 @@
 package ru.kambulov.ticTacToe;
 
+import java.util.function.Predicate;
+
 /**
  * Метода отвечают за логику игры "Крестики-нолики".
  * @author Kambulov Ivan (mailto:kia289@mail.ru)
@@ -12,11 +14,66 @@ public class Logic3T {
     public Logic3T(Figure3T[][] table) {
         this.table = table;
     }
+
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
     /**
      * Метод для логики крестиков.
      * @return возвращает рез-тат проверки поля на наличие победы.
      */
-
+    public boolean isWinnerX() {
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)||
+                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)||
+                this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)||
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)||
+                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)||
+                this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)||
+                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)||
+                this.fillBy(Figure3T::hasMarkX, this.table.length - 1 , 0, -1, 1);
+    }
+    /**
+     * Метод для логики ноликов.
+     * @return возвращает рез-тат проверки поля на наличие победы.
+     */
+    public boolean isWinnerO() {
+        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1) ||
+                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+    }
+    /**
+     * Проверка поля на наличие пустых ячеек.
+     * @return есть ли пустые ячейки на поле?
+     */
+    public boolean hasGap() {
+        boolean result = false;
+        for (int line = 0; line < table.length; line++) {
+            for (int column = 0; column < table.length; column++) {
+                if (!(table[line][column].hasMarkX()) && !(table[line][column].hasMarkO())) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+/*
     public boolean isWinnerX() {
         int line = 0, column = 0;
         boolean result = false;
@@ -40,10 +97,6 @@ public class Logic3T {
         }
         return result;
     }
-    /**
-     * Метод для логики ноликов.
-     * @return возвращает рез-тат проверки поля на наличие победы.
-     */
     public boolean isWinnerO() {
         int line = 0, column = 0;
         boolean result = false;
@@ -67,20 +120,4 @@ public class Logic3T {
         }
         return result;
     }
-    /**
-     * Проверка поля на наличие пустых ячеек.
-     * @return есть ли пустые ячейки на поле?
-     */
-    public boolean hasGap() {
-        boolean result = false;
-        for (int line = 0; line < table.length; line++) {
-            for (int column = 0; column < table.length; column++) {
-                if (!(table[line][column].hasMarkX()) && !(table[line][column].hasMarkO())) {
-                    result = true;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-}
+    */
