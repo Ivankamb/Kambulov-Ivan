25df4b56cedbec53667c85daada465d625f48ab1
Ivan Kambulov <mailto:kia289@mail.ru>
2018-08-27 17:14:21 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.(Task.java)

---
+++ chapter_002/src/main/java/ru/kambulov/models/Task.java
@@ -0,0 +1,18 @@
+package ru.kambulov.models;
+
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version 1.0
+ * @since 27.08.18
+ */
+
+public class Task extends Item {
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String calculatePrice() {
+        return "100%";
+    }
+}
