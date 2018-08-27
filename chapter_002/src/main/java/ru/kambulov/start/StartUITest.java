938461a54c1700eec6647ad6f76336918b5d1e76
Ivan Kambulov <mailto:kia289@mail.ru>
2018-08-27 17:12:46 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.(StartUITest.java)

---
+++ chapter_002/src/main/java/ru/kambulov/start/StartUITest.java
@@ -0,0 +1,16 @@
+package ru.kambulov.start;
+
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version 1.0
+ * @since 27.08.18
+ */
+
+public class StartUITest {
+
+    public static void main(String[] args) {
+        Input input = new StubInput(new String[] {"create stub task"});
+      //  new StartUI(input).init();
+    }
+}
+
