4b9bab5ed9c014355eb0f995d89501aa6acd7985
Ivan Kambulov <mailto:kia289@mail.ru>
2018-08-27 17:16:10 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.(TrackerTest.java)

--- chapter_002/src/test/java/ru/kambulov/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/kambulov/tracker/TrackerTest.java
@@ -1,86 +1,104 @@
 package ru.kambulov.tracker;
 
 import org.junit.Test;
-import static org.junit.Assert.assertThat;
+import ru.kambulov.models.Item;
+import ru.kambulov.start.Tracker;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version 1.0
+ * @since 27.08.18
+ */
 
 public class TrackerTest {
 
-@Test
-public void whenAddNewItemThenTrackerHasSameItem() {
-    Tracker tracker = new Tracker();
-    Item item = new Item("Test name 1", "Test desc 1", 1567L);
-    tracker.add(item);
-    assertThat(tracker.getAll()[0], is(item));
-}
+    @Test
+    public void whenUserGetAllItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = {item1, item2, item3};
+        assertThat(tracker.getAll(), is(expected));
+    }
 
-@Test
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test name 1", "Test desc", "Test comment", 54L);
+        tracker.add(item1);
+        assertThat(tracker.getAll()[0], is(item1));
+    }
+
+    @Test
     public void whenReplaceNameThenReturnNewName() {
-    Tracker tracker = new Tracker();
-    Item before = new Item("Test 1 previous", "Test desc blha blha blha", 98765L);
-    tracker.add(before);
-    Item after = new Item("Test name next", "Test desc one more", 4875L);
-    after.setId(before.getId());
-    tracker.replace(before.getId(), after);
-    assertThat(tracker.findById(after.getId()).getName(), is("Test name next"));
-}
+        Tracker tracker = new Tracker();
+        Item itemBefore = new Item("Test name 1", "Test desc", 456789L);
+        tracker.add(itemBefore);
+        Item itemAfter = new Item("Test name 2", "Test desc", "Comment");
+        itemAfter.setId(itemBefore.getId());
+        tracker.replace(itemBefore.getId(), itemAfter);
+        assertThat(tracker.findById(itemBefore.getId()).getName(), is("Test name 2"));
+    }
 
-@Test
-    public void testFindById() {
-    Tracker tracker = new Tracker();
-    Item item1 = new Item("Name for test 1", "Desc for test 1", 83459L);
-    Item item2 = new Item("Name for test 2", "Desc for test 2", 835629L);
-    Item item3 = new Item("Name for test 3", "Desc for test 3", 2219929L);
-    tracker.add(item1);
-    tracker.add(item2);
-    tracker.add(item3);
-    assertThat(tracker.findById(item2.getId()).getName(), is("Name for test 2"));
-}
+    @Test
+    public void whenDeleteItemThenTrackerWithaoutThisItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test name", "Test desc");
+        Item item2 = new Item("Test name", "Test desc");
+        Item item3 = new Item("Test name", "Test desc");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.delete(item2.getId());
+        Item[] expected = {item1, item3};
+        assertThat((tracker.getAll()), is(expected));
+    }
 
-@Test
-    public void testFindByName() {
-    Tracker tracker = new Tracker();
-    Item item1 = new Item("Name for test 1", "Desc for test 1", 1129L);
-    Item item2 = new Item("Name for test 1", "Desc for test 2", 22L);
-    Item item3 = new Item("Name for test 2", "Desc for test 3", 9999999L);
-    tracker.add(item1);
-    tracker.add(item2);
-    tracker.add(item3);
-    Item[] expected = {item1, item2};
-    assertThat(tracker.findByName(item1.getName()), is(expected));
-}
+    @Test
+    public void whenFindByNameThenTakeAllItemWithThisName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test name1", "Desc", 734L);
+        Item item2 = new Item("Test name2", "desc", 454L);
+        Item item3 = new Item("Test name2", "desc", 77L);
+        Item item4 = new Item("Test name3", "desc", 666L);
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] expected = {item2, item3};
+        assertThat(tracker.findByName(item2.getName()), is(expected));
+    }
 
-@Test
-    public void testGetAll() {
-    Tracker tracker = new Tracker();
-    Item item1 = new Item();
-    Item item2 = new Item();
-    Item item3 = new Item();
-    Item item4 = new Item();
-    tracker.add(item1);
-    tracker.add(item2);
-    tracker.add(item3);
-    tracker.add(item4);
-    Item[] expected = {item1, item2, item3, item4};
-    assertThat(tracker.getAll(), is(expected));
-}
+    @Test
+    public void whenFindByIdThenTakeItemWithThisId() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test name1", "Test desc");
+        Item item2 = new Item("Test name2", "Test desc");
+        Item item3 = new Item("Test name3", "Test desc");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        assertThat(tracker.findById(item2.getId()).getName(), is("Test name2"));
+    }
 
-@Test
-    public void whenDeleteSomeItemThenReturnWithoutThisItem() {
-    Tracker tracker = new Tracker();
-    Item item1 = new Item();
-    Item item2 = new Item();
-    Item item3 = new Item();
-    Item item4 = new Item();
-    Item item5 = new Item();
-    tracker.add(item1);
-    tracker.add(item2);
-    tracker.add(item3);
-    tracker.add(item4);
-    tracker.add(item5);
-    tracker.delete(item2.getId());
-    Item[] expected = {item1, item3, item4, item5};
-    assertThat((tracker.getAll()), is(expected));
-}
+    @Test
+    public void whenUserAddComment() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test name1", "Desc");
+        Item item2 = new Item("Test name2", "Desc");
+        Item item3 = new Item("Test name3", "Desc");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.addComment(item1.getId(), "Test comm");
+        assertThat(tracker.findById(item1.getId()).getComment(), is(" Test comm"));
+    }
 
 }
