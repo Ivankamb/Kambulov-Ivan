bf7e5813e4a2ac483899882c1efbb9e6d4ea2286
Ivan Kambulov <mailto:kia289@mail.ru>
2018-08-27 17:11:03 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.(Input.java)

---
+++ chapter_002/src/main/java/ru/kambulov/start/Input.java
@@ -0,0 +1,12 @@
+package ru.kambulov.start;
+
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version 1.0
+ * @since 27.08.18
+ */
+
+public interface Input {
+    String ask(String question);
+
+}
