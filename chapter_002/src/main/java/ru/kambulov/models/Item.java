43292bf52ae66b923e4f09920bedd027038244ba
Ivan Kambulov <mailto:kia289@mail.ru>
2018-08-27 17:11:37 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.(Item.java)

---
+++ chapter_002/src/main/java/ru/kambulov/models/Item.java
@@ -0,0 +1,74 @@
+package ru.kambulov.models;
+
+/**
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version 1.0
+ * @since 27.08.18
+ */
+
+public class Item {
+    private String comment;
+    private String id;
+    public String name;
+    public String description;
+    public Long create;
+
+    public Item() {
+    }
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Item(String name, String description, String comment) {
+        this.name = name;
+        this.description = description;
+        this.comment = comment;
+    }
+
+    public Item(String name, String description, Long create) {
+        this.name = name;
+        this.description = description;
+        this.create = create;
+    }
+
+    public Item(String name, String description, String comment, Long create) {
+        this.name = name;
+        this.description = description;
+        this.create = create;
+        this.comment = comment;
+    }
+
+    public Long getCreate() {
+        return this.create;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String toString() {
+        return this.name + ". Описание: " + this.description + ". ID " + this.id;
+    }
+}
