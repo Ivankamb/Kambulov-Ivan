1b5e13fcbdff8efdb96c5ad200433230baf3b261
Ivan Kambulov <mailto:kia289@mail.ru>
2018-09-11 20:18:24 +0300

    Рефакторинг - Перенести общие методы в абстрактный класс

--- chapter_002/src/main/java/ru/kambulov/tracker/BaseAction.java
+++ chapter_002/src/main/java/ru/kambulov/tracker/BaseAction.java
@@ -1,5 +1,11 @@
 package ru.kambulov.tracker;
-
+/**
+ * Класс позволяет убрать часть повторяющегося кода из класса MenuTracker.
+ *
+ * @author Kambulov Ivan (mailto:kia289@mail.ru)
+ * @version 1.0
+ * @since 11.09.18
+ */
 public abstract class BaseAction implements UserAction {
     private final int key;
     private final String name;
